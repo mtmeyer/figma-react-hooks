e3ccab1ddd0cc83078eefadf1da97cb4b497c50d
Michael Meyer <michael@mtmeyer.net>
2021-08-19 20:31:25 +1000

    chore: :bulb: Add todo comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import useOnMessage from './useOnMessage';
 import usePostMessage from './usePostMessage';
 
+// TODO: Add hook for window.on();
+
 export { useOnMessage, usePostMessage };
--- src/useOnMessage.ts
+++ src/useOnMessage.ts
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 
-export default function useOnMessage(inputFunction?: (data: any) => void): any {
+// TODO: Add ability to return data without having to pass in a function.
+
+export default function useOnMessage(inputFunction: (data: any) => void): any {
   useEffect(() => {
     window.onmessage = (event) => {
       let data = event.data.pluginMessage;
